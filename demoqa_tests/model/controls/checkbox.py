b168481bec25efc63e3bf93bdd9f290d6be01827
evensty <maxim.veselov11@gmail.com>
2022-12-29 20:39:10 +0300

    fix checkbox control

--- demoqa_tests/model/controls/checkbox.py
+++ demoqa_tests/model/controls/checkbox.py
@@ -2,5 +2,5 @@ from selene import have
 from selene.support.shared import browser
 
 
-def select(selector, by_text):
-    browser.all('[for^=hobbies-checkbox]').element_by(have.exact_text(by_text)).click()
+def select(selector, /, *,  by_text):
+    browser.all(selector).element_by(have.exact_text(by_text)).click()
